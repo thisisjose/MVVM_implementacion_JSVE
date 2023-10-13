d379910ce6853f0aafafba19635dbc5562c689eb
Jose Valenzuela <josecinuwu@gmail.com>
2023-10-12 21:21:51 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_JSVE/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MVVM_implementacion_JSVE.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM_implementacion_JSVE
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new pagina1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MVVM_implementacion_JSVE/VistaModelo/VMpagina1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM_implementacion_JSVE.VistaModelo
+{
+    public class VMpagina1 : BaseViewModel
+    {
+        #region VARIABLES
+        string _N1;
+        string _N2;
+        string _R;
+        #endregion
+        string _Mensaje;
+        
+        #region CONSTRUCTOR
+        public VMpagina1(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+        #region OBJETOS
+        public string Mensjae
+        {
+            get { return _Mensaje; }
+            set { SetValue(ref _Mensaje, value);}
+        }
+        #endregion
+        #region PROCESOS
+        public async Task Procesoasync()
+        {
+
+        }
+        public void Sumar()
+        {
+            double n1 = 0;
+            double n2 = 0;
+            double r = 0;
+
+            n1 = Convert.ToDouble(N1);
+            n2 = Convert.ToDouble(N2);
+            r = Convert.ToDouble(r);
+
+            r = n1 + n2;
+            R = r.ToString();
+        }
+        #endregion //este lo puse yo para que no diera errorXD
+        #region COMANDOS
+        public ICommand Procesoasynccommand => new Command(async () => await Procesoasync());
+        public ICommand Suymarcommand => new Command(Sumar);
+        #endregion
+
+        #region OBJETOS
+        public string N1
+        {
+            get { return _N1; }
+            set { SetValue(ref _N1, value); }
+        }
+        public string N2
+        {
+            get { return _N2; }
+            set { SetValue(ref _N2, value); }
+        }
+        public string R
+        {
+            get { return _R; }
+            set { SetValue(ref _R, value); }
+        }
+        #endregion
+
+
+    }
+}
+
